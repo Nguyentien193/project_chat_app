0047e4a4e0c52ac9203afb6a9af2b9b9335c147e
tiennv <tien.nguyen@bnksolution.com>
2025-06-18 19:47:10 +0700

    update

--- html/utils/env.ts
+++ html/utils/env.ts
@@ -4,5 +4,5 @@ const TOKEN_KEY = 'tokenCookie';
 export const APP_CONFIG: any = {
   profileKey: USER_PROFILE_KEY,
   tokenKey: TOKEN_KEY ,
-  apiUrl: 'https://webchatapp-weqg.onrender.com/api',
+  apiUrl: 'http://backend.congnghetheodoi24h.com/api',
 };
